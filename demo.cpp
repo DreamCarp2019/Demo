e3c409a9d650d20b524a72b85d42c53c9a9400af
lmc <2461383396@qq.com>
2020-12-15 10:37:15 +0800

    第二次修改

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+class People
+{
+public:
+People(){}
+People(int age,string name):age(age),name(name){}
+void print()
+{
+   cout<<age<<"\t"<<name<<endl;
+}
+protectd:
+int age;
+string name;
 
 int main()
 {
+   People p1(18,"allen");
+   p1.print();
    cout<<"this is a cpp demo"<<endl;
    return 0;
+	
 }
